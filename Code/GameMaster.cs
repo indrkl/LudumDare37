aefec18e6f7986f39a4f29467286361939fae97a
indrkl <indrkl@gmail.com>
2016-12-10 13:42:06 +0200

    Initial pieces of code.

--- Code/GameMaster.cs
+++ Code/GameMaster.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public int maxLevel;
+	public int curLevel;
+
+	public Player player;
+	public Transform playerSpawn;
+
+	public List<Obstacle> obstacles;
+
+	public void InitializeGame (){
+		deathCounter = 0;
+		curLevel = 1;
+		StartLevel (maxLevel);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void StartLevel(int levelNumber){
+		foreach(Obstacle o in obstacles){
+			if (o.fromLevel <= levelNumber) {
+				o.gameObject.SetActive (true);
+			} else {
+				o.gameObject.SetActive (false);
+			}
+		}
+
+		player.transform.position = playerSpawn.position;
+	}
+
+	public int deathCounter;
+
+	public void OnDeath(){
+		deathCounter++;
+		StartLevel (curLevel);
+	}
+
+	public void OnSuccess(){
+		curLevel++;
+		if (curLevel > maxLevel) {
+			WinGame ();
+		} else {
+			StartLevel (curLevel);
+		}
+	}
+
+	public void WinGame(){
+
+	}
+
+	void Start(){
+		InitializeGame ();
 	}
 }
---
+++ Code/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float maxRunSpeed;
+	public float acceleration;
+	public float rightDirection;
+
+	bool grounded = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float curSpeed = GetComponent<Rigidbody> ().velocity.x;
+		if (Input.GetKey (KeyCode.D)) {
+			if(grounded)
+				GetComponent<Rigidbody> ().AddForce ((1 - (Mathf.Max(curSpeed, 0)/maxRunSpeed)) * acceleration, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			if(grounded)
+				GetComponent<Rigidbody> ().AddForce ((1 - Mathf.Max(-curSpeed, 0)/maxRunSpeed) * acceleration * -1, 0, 0);
+		}
+	}
+}
