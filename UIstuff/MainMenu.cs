990e8c1c07baac91db061dd400baa52ece1f8c0d
indrkl <indrkl@gmail.com>
2016-12-11 13:49:03 +0200

    Added MainMenu Script

---
+++ UIstuff/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public string gameSceneName;
+
+	public void startGame(){
+		SceneManager.LoadScene (gameSceneName);
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+	}
+
+
+}
