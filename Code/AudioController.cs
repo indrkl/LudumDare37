6e0fc2db0abbe73b1acc59ea334b179c661e3fe9
indrkl <indrkl@gmail.com>
2016-12-12 20:08:20 +0200

    merge

--- Code/AudioController.cs
+++ Code/AudioController.cs
@@ -14,7 +14,7 @@ public class AudioController : MonoBehaviour {
 	}
 
 	public List<AudioClip> levelStartClips;
-	public AudioClip deathClip;
+	public List<AudioClip> deathClip;
 	List<bool> playedClip;
 
 	public AudioClip startMaxLevel;
@@ -23,20 +23,27 @@ public class AudioController : MonoBehaviour {
 
 	public void startLevel(int level){
 		if (levelStartClips.Count >= level && playedClip [level - 1] == false) {
-			GameMaster.instance.GetComponent<AudioSource> ().clip = levelStartClips [level - 1];
-			GameMaster.instance.GetComponent<AudioSource> ().Play ();
+			StartCoroutine (playCongAndMessage (levelStartClips[level - 1]));
 			playedClip [level - 1] = true;
-		} else {
-			GameMaster.instance.GetComponent<AudioSource> ().clip = deathClip;
-			GameMaster.instance.GetComponent<AudioSource> ().Play ();
+		} else if (deathClip.Count > 0){
+			StartCoroutine (playCongAndMessage (deathClip[Random.Range(0, deathClip.Count)]));
 		}
 	}
 
 	public void playStartMaxLevel(){
 		if (startMaxLevel) {
-			GameMaster.instance.GetComponent<AudioSource> ().clip = startMaxLevel;
-			GameMaster.instance.GetComponent<AudioSource> ().Play ();
+			StartCoroutine (playCongAndMessage (startMaxLevel));
 		}
+	}
+
+	public AudioClip cong;
+
+	public IEnumerator playCongAndMessage(AudioClip msg){
+		GetComponent<AudioSource> ().clip = cong;
+		GetComponent<AudioSource> ().Play ();
+		yield return new WaitForSeconds (2);
+		GetComponent<AudioSource> ().clip = msg;
+		GetComponent<AudioSource> ().Play ();
 	}
 
 
